7be61b5d43b4ff884282ee94f51c5af2cc45dce6
RandLily <48348945+RandLily@users.noreply.github.com>
2022-08-10 15:23:06 +0800

    Update debugger.py

--- debugger.py
+++ debugger.py
@@ -1,105 +1,71 @@
-
-# by 20000s 2022.5.3
-from adbutils import adb
 from sqlalchemy import true
-import os
 import threading
-import time
-import random
-PACKAGE_NAME = ""
-SO_NAME = ""
-DIR_PATH = "/data/local/tmp/"
+import idaapi
+import subprocess
 
+def adb_connect(ip,port=None):
+    if port!=None:
+        cmd="adb connect "+ip+":"+str(port)
+    else:
+        cmd="adb connect "+ip
+    subprocess.run(cmd,shell=True)
 
+def adb_input(string):
+	cmd="adb shell input text "+str(string)
+	subprocess.run(cmd,shell=True)
+        
+def start_server(name):
+    cmd='start adb shell "su -c \'./data/local/tmp/'+name+"'"
+    subprocess.Popen(cmd,shell=True)
+    
 def jdb_connect():
-    os.system("jdb -connect com.sun.jdi.SocketAttach:hostname=127.0.0.1,port=8700")
+    subprocess.Popen("jdb -connect com.sun.jdi.SocketAttach:hostname=localhost,port=17788",shell=True)
 
+def cat_maps(name,pid):
+    cmd="adb shell "
+    cmd+='"su -c '+"'cat /proc/" + str(pid) + "/maps | grep " + name + "'\""
+    return subprocess.getoutput(cmd)
 
-def start_debug(package_name,so_name):
-    PACKAGE_NAME = package_name
-    SO_NAME = so_name
-    print("--------------start debugging------------------------")
-    d = adb.device()
-    adb.forward(d.serial,"tcp:23946","tcp:23946")
-#    for item in adb.forward_list():
-#        print(item.serial, item.local, item.remote)
-
-    d.shell("am set-debug-app -w " + PACKAGE_NAME)
-    print("plz click " + PACKAGE_NAME)
-    while true:
-        tmp = d.shell("ps -A | grep " + PACKAGE_NAME)
-        if( PACKAGE_NAME in tmp):
+#mode：0为attach模式(默认)，1为spawn模式(按需)
+#port：ida端口，默认为23976
+def start_debug(package_name,mode=0,port=23946,brk=0):
+    so_name = idaapi.get_root_filename()
+    print("------------target is "+so_name+"------------")
+    subprocess.run("adb forward tcp:"+str(port)+" tcp:"+str(port),shell=True)
+    #spawn模式可能会出现获取不到基地址情况，按需使用
+    if mode==1:
+        subprocess.run("adb shell am set-debug-app -w " + package_name,shell=True)
+        print("[*]spwan mode")
+    else:
+        print("[*]attach mode")
+    print("[*]plz click " + package_name)   
+    cmd="ps -A | grep " + package_name
+    while True:
+        tmp=subprocess.getoutput("adb shell "+'"'+cmd+'"')
+        if(package_name in tmp):
             break
-  
-    print(PACKAGE_NAME + "has been open")
-    line1 = d.shell("ps -A | grep " + PACKAGE_NAME)
-    line1_list = line1.split()
-    pid = int(line1_list[1])
-    print(PACKAGE_NAME + " pid :" + str(pid))
-    adb.forward(d.serial,"tcp:8700","jdwp:"+str(pid))
-    print("plz use ida to attach "+ PACKAGE_NAME)
-    ida_dbg.set_remote_debugger("127.0.0.1",None)
+    tmp = tmp.split()
+    pid = int(tmp[1])
+    print("[*]"+package_name + " pid :" + str(pid))
+    subprocess.run("adb forward tcp:17788"+" jdwp:"+str(pid),shell=True)
+    ida_dbg.set_remote_debugger("localhost",None)
     attach_process(pid,1)
-    print("debugger on")
     set_debugger_options(DOPT_LIB_BPT | DOPT_THREAD_BPT)
     t1 = threading.Thread(target = jdb_connect)
     t1.start()
-    print("ok")
-    # adbutil无root fuck
-    # CMD = "cat /proc/" + str(pid) + "/maps | grep " + SO_NAME
-    # BASE = ""
-    # while true:
-    #     tmp = d.shell(CMD)
-
-    #     if(tmp):
-    #         print(tmp)
-    #         tmp = tmp.split()
-    #         for i in range(len(tmp)):
-    #             if tmp[i] == "r-xp":
-    #                 BASE = tmp[i-1]
-    #                 break
-    #         BASE = BASE.split("-")[0]
-    #         BASE = int(BASE,16)
-    #         break
-
-    #     else:
-    #         ida_dbg.continue_process()
-    # print(BASE)
-    ida_dbg.continue_process()
-    ida_dbg.wait_for_next_event(WFNE_SUSP,1)
-    has_so = False
-    while True:
-        module_base = get_first_module()
-        while module_base != None:
-            module_name = get_module_name(module_base)
-            if module_name.find(SO_NAME) >= 0:
-                has_so = True
-                break
-        
-            module_base = get_next_module(module_base)
-        if has_so:
+    #某些SO在IDA中不以文件名显示，而在apk中
+    r=cat_maps(so_name,pid)
+    if r=='':
+    	r=cat_maps(package_name,pid)
+    tmp=r.split()
+    base=0
+    for i in range(len(tmp)):
+        if tmp[i] == "r-xp":
+            base = tmp[i-1]
+            base = base.split("-")[0]
+            base = int(base, 16)
             break
-        else : 
-            ida_dbg.continue_process()
-            ida_dbg.wait_for_next_event(WFNE_SUSP,1)  
-    module_size = get_module_size(module_base)
-    print('[*]found so base=>0x%08X, Size=0x%08X' % (module_base, module_size))
-
-def dump(ea_start,ea_end):
-    print('[*]begin to dump segment')
-    save_file = DIR_PATH + PACKAGE_NAME + str(random.randint(0,10000))
-    handle_f = open(save_file, 'wb')
-    for byte_addr in range(ea_start, ea_end):
-        byte_value = idaapi.get_byte(byte_addr)
-        handle_f.write(struct.pack('B',byte_value))
-    
-    handle_f.close()
-    hooks = idaapi.DBG_Hooks()
-    hooks.hook()
-    print("[*] saved in " + save_file)
-
-
-
-
-    
-
+    print('[*]found so base:'+hex(base))
+    if brk!=0 and base!=0:
+        idaapi.add_bpt(base+brk)
+        print('[*]set breakpoint at:'+hex(base+brk))
